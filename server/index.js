1897e15ca9d56f29d2e51ea8810541736c77f174
Manisha11sep <er.manisha11sep@gmail.com>
2018-05-09 11:58:59 -0700

    Getting data from API

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const graphqlHTTP = require('express-graphql')
 const PORT = 3050
 
 const app = express()
+const schema = require('./graphql/schema')
 
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use('/graphql',graphqlHTTP({
+    schema:schema,
+    graphiql:true
+}))
 app.listen(PORT, () => console.log('Listening on Port: ' + PORT))
